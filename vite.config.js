297bc89ede75c8ece81a09158efa87b593d3258b
Adityamkumar <adityasharma841460@gmail.com>
2025-12-16 13:59:52 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: ["iphone-17-co.onrender.com"],
+  },
 })
